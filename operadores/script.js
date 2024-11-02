12825ce72dcfd47e6c685d213ff04c090282a864
FelipeLohan <felipelohan03052006@gmail.com>
2024-11-02 16:39:35 -0300

    Anotação operadores feita

---
+++ operadores/script.js
@@ -0,0 +1,32 @@
+/*
+
+Operadores Aritméticos:
+
++
+-
+*
+/
+%
+** (Exponenciação)
+
+Operadores Comparativos:
+
+<
+>
+<=
+>=
+
+== (Compara somente o valor)
+=== (Compara o tipo e o valor)
+
+object (Compara a referência dele na memória)
+
+string,number,boolean (Comparam o valor)
+
+Comparadores Logicos:
+
+&&
+||
+!
+
+*/
