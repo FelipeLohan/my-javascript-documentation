4efa8903589d3f1ab1e4f854808c03e741c03324
FelipeLohan <felipelohan03052006@gmail.com>
2024-11-05 10:50:46 -0300

    Falta completar antações sobre Json

---
+++ json/script.js
@@ -0,0 +1,25 @@
+
+let precoTotal = 0
+const obj1 = {
+  nome: 'Lohan',
+  idade: 18,
+  sexo: 'Masculino',
+  saldo: 1800,
+  pedido: [
+    {
+      produto: 'carro',
+      preco: 43000,
+      'ano-produto': 2012
+    },
+
+    {
+      produto: 'moto',
+      preco: 2500,
+      'ano-produto': 2015
+    }
+  ],
+  total: 1300
+  
+}
+
+console.log(obj1.pedido)
