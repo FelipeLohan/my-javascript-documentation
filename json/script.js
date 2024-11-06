61f0a3836425c87349996e2531f58a5bd5de75b6
FelipeLohan <felipelohan03052006@gmail.com>
2024-11-06 08:48:39 -0300

    Anotações feitas sobre JSON

--- json/script.js
+++ json/script.js
@@ -1,25 +1,61 @@
 
-let precoTotal = 0
+//Isso é um JSON, ele é instanciado dessa forma, entre chaves e cada variavel é separada por (nome: valor)
+//no caso abaixo podemos ver que o nome das variaveis está em String, o que pode ocorrer.
 const obj1 = {
-  nome: 'Lohan',
-  idade: 18,
-  sexo: 'Masculino',
-  saldo: 1800,
-  pedido: [
-    {
-      produto: 'carro',
-      preco: 43000,
-      'ano-produto': 2012
-    },
-
-    {
-      produto: 'moto',
-      preco: 2500,
-      'ano-produto': 2015
-    }
-  ],
-  total: 1300
-  
+  "name": "Computador",
+  "price": 50.9,
+  "due-date": "2025-04-15"
 }
 
-console.log(obj1.pedido)
+//aqui no obj2 o Json está com as variaveis em String e normal
+const obj2 = {
+  name: "Computador",
+  price: 50.9,
+  "due-date": "2025-04-15"
+}
+
+//aqui no obj3 podemos ver que instanciamos um Array de Objetos dentro do items, que é algo que podemos fazer dentro de um json.
+const obj3 = {
+  id: 53,
+  date: "2021-10-20",
+  items: [
+      {
+          description: "Celular",
+          price: 1499.99,
+          quantity: 1
+      },
+      {
+          description: "Mouse",
+          price: 100.0,
+          quantity: 2
+      }
+  ],
+  client: {
+      name: "Maria Red",
+      email: "maria@gmail.com",
+      active: true
+  }
+};
+
+//metodos de acesso ao Json
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+
+//podemos acessar as variaveis dentro do Json, e ainda as variaveis dentro das variaveis do Json
+console.log(obj3.id);
+console.log(obj3.client);
+console.log(obj3.client.name);
+
+//Muitas vezes a gente pode pegar um JSON do banco de dados ou de alguma aplicação que fornece para gente, e pode vir desse modo, totalmente em String.
+//podemos fazer a conversão dessa String pra um JSON, usando o JSON.parse(nomeDaVariavel) e armazenando esse metodo em outra variavel.
+//como no caso abaixo
+const txt = `{"nome": "Computador", "price": 50.9, "due-date": "2025-04-15"}`;
+
+const obj = JSON.parse(txt);
+
+console.log(txt.name);
+
+console.log(obj.nome);
+
+const text = JSON.stringify(obj); //nesse caso ele ta convertendo os dados JSON em String novamente, provavelmente para ser mandado via protocolo http.
