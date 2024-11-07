187d34c9ce1be6a823107d9d8a85a1999180c407
Felipe Lohan <felipelohan03052006@gmail.com>
2024-11-07 15:25:46 -0300

    Anotações acrescentadas, falta complementar e estudar mais

--- desestruturacao/script.js
+++ desestruturacao/script.js
@@ -1,16 +1,30 @@
+//A desestruturação serve para extrair alguma propriedade ou elemento de um objeto ou array.
+//A técnica além de simplificar o código deixa ele mais legivel e reduz a quantidade de linhas
+
+const array = [1, 2, 3, 4]
+const [um , dois, tres, quatro] = array //isso é a desestruturação de um array, aqui eu dei um nome a cada item do array de acordo com seu index
+
+console.log('--------ARRAY:-------')
+console.log(um)
+
+
+//--------------------
+//objeto:
+
 const pessoa = {
-    name: 'Felipe',
-    age: 18
+    nome: 'Lohan',
+    idade: '18',
+    sexo: 'Masculino'
 }
 
-const pessoa2 = {
-    name: 'Carlos',
-    age: 32
-}
+const { nome, idade, sexo } = pessoa //nesse caso criei um objeto e peguei o nome das variaveis que quero acessar por fora, sem ter que chamar o obj
 
-const { name, age } = pessoa
-console.log(name, age)
+console.log('--------OBJETO:-------')
+console.log(nome)
+console.log(idade)
+console.log(sexo)
 
-const  { name: name1, age: age1 } = pessoa2
-
-console.log(name1, age1)
+//sem a desestruturação isso seria acessado dessa forma:
+pessoa.nome
+pessoa.idade
+pessoa.sexo
